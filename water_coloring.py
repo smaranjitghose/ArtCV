a96c9841dbb4a3edf222e3f67745b63e31db3983
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-08-01 22:16:49 +0530

    Made Water Coloring more Verbose

--- water_coloring.py
+++ water_coloring.py
@@ -11,9 +11,16 @@ args = vars(ap.parse_args())
 img = cv2.imread((args["image"]))
 
 water_color_img  = cv2.stylization(img, sigma_s = 60,sigma_r = 0.6)
-# Where signma_s is used for controlling the size of the neighbourhood. It's value are from 1-200
-# signma_r is used to control how the dissimilar colors within the neighbourhood will be averaged. It's value range from 0 to 1
-# A larger value of sigma_r can lead to to large regions of constant color
+# The smoothing filter replaces the value of a pixel by the weighted sum of its neighbors
+# The bigger the neighbourhood, the smoother the image becomes
+# The parameter signma_s(Sigma_Spatial) is used for controlling the size of the neighbourhood.
+# It's value are from 1-200
+
+# At times it is not possible to replace the color of a pixel by the weighted sum of its neighbors.
+# Rather color value at a pixel is repaced by the average of pixels in the neighborhood which also have color similar to the pixel.
+# The parameter sigma_r(Sigma_Range) is used for controlling the averaging of dissimilar colors within the neighbourhood.
+# It's value range from 0 to 1
+# A larger value of sigma_r results in large regions of constant color.
 
 #create Window to display images
 cv2.imshow('Water Coloring', water_color_img)
