b6202ab27d28e661cf035da3b2534b8cd51e3001
Preetesh Verma <verma.preetesh21@gmail.com>
2020-09-18 17:34:30 +0530

    adding function

--- stipple.py
+++ stipple.py
@@ -5,6 +5,20 @@ import random
 from PIL import Image,ImageColor
 import math
 
+def stippler(imgNew,width,height):
+    # here performing the operation where we are randomly selecting the pixel and adding the effect.
+    for x in range(width):
+        for y in range(height):
+            # here we extract the pixel value of the image.
+            point = img.getpixel( (x,y) )[0]
+            # generating a random value
+            randNum = random.randint(0, 260)
+            # condition for manipulating the particular pixels value.
+            if randNum >= point:
+                # changing the value of the pixel.
+                imgNew.putpixel( (x,y), (0, 255))
+    return imgNew
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
@@ -18,16 +32,6 @@ width, height = img.size
 # here we are converting the image to the greyscale form('L') with an alpha ('A') transparency
 imgNew = Image.new('LA', (width, height))
 
-# here performing the operation where we are randomly selecting the pixel and adding the effect.
-for x in range(width):
-    for y in range(height):
-        # here we extract the pixel value of the image.
-        point = img.getpixel( (x,y) )[0]
-        # generating a random value
-        randNum = random.randint(0, 260)
-        # condition for manipulating the particular pixels value.
-        if randNum >= point:
-            # changing the value of the pixel.
-            imgNew.putpixel( (x,y), (0, 255))
+imgNew=stippler(imgNew,width,height)
             
 imgNew.save('assets/Stippled.png')
