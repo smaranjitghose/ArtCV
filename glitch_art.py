60b0d1381b4f190be96824bf8d239de1d05a0d56
Preetesh Verma <verma.preetesh21@gmail.com>
2020-10-11 00:28:04 +0530

    readme update

--- glitch_art.py
+++ glitch_art.py
@@ -11,6 +11,7 @@ def gen_divisors(n):
     if n % i == 0:
       yield i
 
+# generating the extedned padded image
 def pad_to_square(imgobj):    
       """Pad to the nearest 100th"""
       square = Image.new('RGB', ((imgobj.width // 100 + 1) * 100,(imgobj.height // 100 + 1) * 100), (0, 0, 0))
@@ -18,18 +19,20 @@ def pad_to_square(imgobj):
       square.paste(imgobj, imgobj.getbbox())
       return square
 
+# the function returns the glitched image with given step size
 def pixel_sort(imgobj, step_size=8):
       # padding the image to transform into square
       padded = pad_to_square(imgobj)
       # image to array
       data = np.array(padded)
-      # dividing the image into stripes for merging
+      # dividing the image into stripes
       stripes = np.split(data, data.shape[0] // step_size, axis=0)
       sorted_data = []
       # sort by rows
       for stripe in tqdm(stripes):
         sorted_data.append(np.sort(stripe, axis=0))
       sorted_arr = np.array(sorted_data)
+      # shaping the sorted array into the padded image shape
       sorted_arr = sorted_arr.reshape(padded.height, padded.width, 3)
       # remove padding
       sorted_arr = sorted_arr[:imgobj.height, :imgobj.width, :]  
@@ -48,8 +51,7 @@ def glitch_image(fname):
       idx = np.random.choice(len(divisors))
       step_size = divisors[idx]
       try:
-        return pixel_sort(original, step_size=step_size)
-      
+        return pixel_sort(original, step_size=step_size)      
       except:
         print('Dimension errors processing ' + fname + ' Please try again.')
         return None
