439062cca268f114d460d5d5242d9b4218e134ea
RawX18 <68515436+RawX18@users.noreply.github.com>
2020-10-18 14:34:59 +0530

    Update cartoon_effects.py

--- cartoon_effects.py
+++ cartoon_effects.py
@@ -1,49 +1,34 @@
 import cv2 #image processing lib
 import numpy as np #matrix manipulation
-import time 
-
-def comic(img):
-    # do edge detection on a grayscale image
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
-    edgesOnly = cv2.blur(grayImg, (3, 3)) # this blur gets rid of some noise
-    edgesOnly = cv2.Canny(edgesOnly, 50, 150, apertureSize=3) # this is the edge detection
-
-    # the edges are a bit thin, this blur and threshold make them a bit fatter
-    kernel = np.ones((3,3), dtype=np.float) / 12.0
-    edgesOnly = cv2.filter2D(edgesOnly, 0, kernel)
-    edgesOnly = cv2.threshold(edgesOnly, 50, 255, 0)[1]
-
-    # convert to colour...
-    edgesOnly = cv2.cvtColor(edgesOnly, cv2.COLOR_GRAY2BGR)
-
-    # this operation blurs things but keeps track of
-    # colour boundaries
-    shifted = cv2.pyrMeanShiftFiltering(img, 5, 20)
-
-    # now compose with the edges, the edges are white so take them away
-    # to leave black
-    return cv2.subtract(shifted, edgesOnly)
+from time import sleep #for slowing down the process to make progress visible 
+from tqdm import tqdm as tqdm # for progess bar 
 
 def cartoonize(img):
-    # 1) Edges -> xonvert the image to gray scale and blur it using a medianBlur (blurring method).
-    # Now apply the adaptiveThresholding to pull out highlighted object boundaries.
-    
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    grayImg = cv2.medianBlur(grayImg, 5)
-    edgesOnly = cv2.adaptiveThreshold(grayImg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 19, 6)
-    
-    # 2) Color
-    color = cv2.bilateralFilter(img, 9, 30, 30)
-    color1 = cv2.medianBlur(color,25)
-    
-    
-    # 3) Cartoon
-    cartoon = cv2.bitwise_and(color1, color1, mask=edges)
-    return cv2.medianBlur(cartoon,3),edgesOnly
- 
-          
-# Driver's code 
-#img = cv2.imread("assets/kolkata.jpg")
+    sleep(0.1)
+    with tqdm(total=100,desc="Progress") as pbar1:
+        # 1) Edges -> xonvert the image to gray scale and blur it using a medianBlur (blurring method).
+        # Now apply the adaptiveThresholding to pull out highlighted object boundaries.
+        grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        grayImg = cv2.medianBlur(grayImg, 5)
+        # Image Blur using Median Blur of filter size 5
+        sleep(0.2)# added sleep function inorder to make the progress visisble on the bar as without it processing might be
+        # fast and will give 100% progess as soon as we put the filter/function in effect.
+        pbar1.update(25)
+        edgesOnly = cv2.adaptiveThreshold(grayImg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 19, 6)
+        sleep(0.2)
+        pbar1.update(30)
+        # 2) Color output using bilateralFilter
+        color = cv2.bilateralFilter(img, 9, 30, 30)
+        color1 = cv2.medianBlur(color,25)
+        #Again blur the image with filter size of 25
+        sleep(0.2)
+        pbar1.update(25)
+
+        # 3) Cartoon (perform bitwise operation with edge mask)
+        cartoon = cv2.bitwise_and(color1, color1, mask=edgesOnly)
+        sleep(0.2)
+        pbar1.update(20)
+        return cv2.medianBlur(cartoon,3),edgesOnly
 
 #construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -56,10 +41,9 @@ img = cv2.imread((args["image"]))
 print("Wait, Work is in Progess.")
 
 res_img1,res_img2 = cartoonize(img)
-res_img3 = comic(img)
+
 cv2.imwrite("assets/cartoon1.jpg", res_img1)
 cv2.imwrite("assets/black_and_wihte_cartoon.jpg", res_img2)
-cv2.imwrite("assets/comic_cartoon_effect.jpg", res_img3)
 
 
 print("Your results are ready!")
